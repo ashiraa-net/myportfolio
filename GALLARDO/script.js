6dcbd150b768f82d66972687337dce2e30409809
ashiraa-net <rasserugyararudo08@gmail.com>
2025-04-01 16:37:23 +0800

    Delete GALLARDO directory

--- GALLARDO/script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll for navigation links
-document.querySelectorAll("nav ul li a").forEach(link => {
-    link.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
